83cbcdce46f4f26ee1a993663db48aa9fdcac8db
jorgeortiz41 <jorge.ortiz41@upr.edu>
2026-08-21 10:15:28 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+fn detect_command(command: &str) -> &str {
+    match command.find(' ') {
+        Some(index) => &command[0..index],
+        None => command, // no space? the whole thing is the command
+    }
+}
+
 fn main() {
     loop {
         // Initialize Shell and read input
@@ -12,16 +19,20 @@ fn main() {
             .expect("Failed to read line");
         let command = command.trim();
 
-        // Quit if command is quit
-        if command == String::from("exit") {
-            break;
-        }
+        // Detect Command
+        let parsed_command = detect_command(&command);
 
-        // Future: Handle "valid" command condition
-        //
-        //
-        // Print output
-        eprintln!("{}: command not found", command);
+        // Handle parsed command
+        if parsed_command == String::from("exit") {
+            // Exit if command is "exit"
+            break;
+        } else if parsed_command == String::from("echo") {
+            // Echo the rest of line after "echo" command
+            println!("{}", &command[5..]);
+        } else {
+            // Command not found error
+            eprintln!("{}: command not found", command);
+        }
         io::stdout().flush().unwrap();
     }
 }
