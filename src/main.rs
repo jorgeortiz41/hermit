d8b4b248661337d299b13c32372379f6de02c679
jorgeortiz41 <jorge.ortiz41@upr.edu>
2026-08-21 02:10:36 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,26 @@
 use std::io::{self, Write};
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        // Initialize Shell and read input
+        print!("$ ");
+        io::stdout().flush().unwrap();
+        let mut command = String::new();
+        io::stdin()
+            .read_line(&mut command)
+            .expect("Failed to read line");
+        let command = command.trim();
+
+        // Quit if command is quit
+        if command == String::from("quit") {
+            break;
+        }
+
+        // Future: Handle "valid" command condition
+        //
+        //
+        // Print output
+        eprintln!("{}: command not found", command);
+        io::stdout().flush().unwrap();
+    }
 }
